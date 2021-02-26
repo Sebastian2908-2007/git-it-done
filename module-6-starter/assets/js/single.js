ec607bf415ae12a724e561ef9e0d9ce9078c19ab
sebastian <sebastian bowen>
2021-02-26 12:20:46 -0700

    issues done

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,7 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container")
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name"); 
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo name on page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }else {
+        // if no repo was given redirect to homepage
+        document.location.replace("./index.html");
+    }
+
+};
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?directions=asc";
+  
 
   fetch(apiUrl).then(function(response) {
       // request was successful
@@ -18,7 +33,8 @@ var getRepoIssues = function(repo) {
           });
       }
       else {
-          alert("There was a problem with your request!");
+          // if not succesfull redirect o front page
+        document.location.replace("./index.html")
       }
   });
    
@@ -69,4 +85,5 @@ linkEl.setAttribute("target", "_blank");
 // append to warning container
 limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
